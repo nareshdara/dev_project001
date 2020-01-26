04f56f49c656214f6c0974b8c4620aa54f1de21a
naresh <nareshdara200@gmail.com>
2020-01-26 11:14:42 +0530

    devmessages

---
+++ dev.java
@@ -0,0 +1,4 @@
+this is for dev1 project files for this location
+so we are able to execute in this given point of time
+we are able to execute in this point of time
+so we are able to execute test result.
